19d304a575e099c8da234b3f9361f0a4bac5d740
NazarovAn <naatonchrome@gmail.com>
2020-08-21 12:12:25 +0400

    edit remove check

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(async (ctx, next) => {
 });
 
 let tickets = [];
-const fullTickets = [];
+let fullTickets = [];
 
 app.use(async (ctx) => {
   const { method } = ctx.request.query;
@@ -75,6 +75,59 @@ app.use(async (ctx) => {
     return newTicket;
   }
 
+  function removeTicket(context) {
+    const { ticketId } = context.request.query;
+    try {
+      tickets = tickets.filter((item) => item.id !== ticketId);
+      fullTickets = fullTickets.filter((item) => item.id !== ticketId);
+
+      return { removed: true };
+    } catch (err) {
+      return { removed: false , err };
+    }
+  }
+
+  function checkTicket(context) {
+    const { ticketId } = context.request.query;
+
+    try {
+      const ticket = tickets.find((item) => item.id === ticketId);
+      if (ticket.status === false) {
+        ticket.status = true;
+      } else {
+        ticket.status = false;
+      }
+
+      const fullTicket = fullTickets.find((item) => item.id === ticketId);
+      if (fullTicket.status === false) {
+        fullTicket.status = true;
+      } else {
+        fullTicket.status = false;
+      }
+      return { done: true, status: ticket.status };
+    } catch (err) {
+      return { done: false, err };
+    }
+  }
+
+  function editTicket(context) {
+    const data = JSON.parse(context.request.body);
+    const { name, description, id } = data;
+
+    try {
+      const ticket = tickets.find((item) => item.id === id);
+      ticket.name = name;
+
+      const fullTicket = fullTickets.find((item) => item.id === id);
+      fullTicket.name = name;
+      fullTicket.description = description;
+
+      return { edited: true, name, description };
+    } catch(err) {
+      return { edited: false, err };
+    }
+  }
+
   switch (method) {
     case 'allTickets':
       ctx.response.body = tickets;
@@ -84,6 +137,15 @@ app.use(async (ctx) => {
       return;
     case 'createTicket':
       ctx.response.body = createTicket(ctx);
+      return;
+    case 'removeTicket':
+      ctx.response.body = removeTicket(ctx);
+      return;
+    case 'checkTicket':
+      ctx.response.body = checkTicket(ctx);
+      return;
+    case 'editTicket':
+      ctx.response.body = editTicket(ctx);
       return;
     default:
       ctx.response.status = 404;
